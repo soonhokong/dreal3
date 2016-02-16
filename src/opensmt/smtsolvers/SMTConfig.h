3178454182a078b9002fc039006e88328d0ad49c
Soonho Kong <soonhok@cs.cmu.edu>
2016-02-16 16:01:15 -0500

    feat(opensmt/smtsolvers/SMTConfig): add --sat-icp option

--- src/opensmt/smtsolvers/SMTConfig.h
+++ src/opensmt/smtsolvers/SMTConfig.h
@@ -219,6 +219,7 @@ struct SMTConfig
   bool         nra_polytope;                  // use polytope contractor in IBEX
   bool         nra_simp;                      // use simplification in preprocessing
   bool         nra_ncbt;                      // use nonchronological backtracking in icp
+  bool         nra_sat_icp;                   // use sat+icp algorithm
   bool         nra_local_opt;                 // use local optimization to refine counter example (for exist-forall problems)
   bool         nra_worklist_fp;               // use worklist fixpoint algorithm
   bool         nra_multiprune;                // try the top k dimensions to branch on, and see which contract the most before selecting a branch
